ff4c3fd568da2283c9da5bee2e8216b7d33ee352
paalamugan <paalamugan@gmail.com>
2022-11-01 00:30:16 +0530

    update 2_functional_tests integration test cases

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -50,9 +50,10 @@ suite('Functional Tests', function() {
       });
     });
     //After all tests are finished drop database and close connection
-    after(async function(done){
-      await mongoose.connection.db.dropDatabase();
-      mongoose.connection.close(done);
+    after(function(done){
+      mongoose.connection.db.dropDatabase(function(){
+        mongoose.connection.close(done);
+      });
     });
 
     suite('POST /api/books with title => create book object/expect book object', function() {
